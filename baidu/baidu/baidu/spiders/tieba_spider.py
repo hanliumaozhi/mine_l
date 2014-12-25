c2182c803a6f0f5c3b37131f85f80f479f346792
hanliumaozhi <hanliumaozhi@gmail.com>
2014-12-26 07:31:36 +0800

    add function

--- baidu/baidu/baidu/spiders/tieba_spider.py
+++ baidu/baidu/baidu/spiders/tieba_spider.py
@@ -3,14 +3,15 @@
 import scrapy
 from scrapy.selector import Selector
 from baidu.items import BaiduItem
+from scrapy.http.request import Request
 
 
 class tiebaSpider(scrapy.Spider):
     name = "tieba"
     allowed_domains = ["baidu.com"]
-    start_urls = [
-        "http://tieba.baidu.com/f?kw=%E6%B1%89%E6%9C%8D%E6%B0%B4%E5%90%A7&ie=utf-8&pn=0"
-    ]
+    start_urls = ['http://tieba.baidu.com/f?kw=%E6%B1%89%E6%9C%8D%E6%B0%B4%E5%90%A7&ie=utf-8&pn=0']
+    pre_url = "http://tieba.baidu.com/f?kw=%E6%B1%89%E6%9C%8D%E6%B0%B4%E5%90%A7&ie=utf-8&pn="
+    index_url = 0
     
     def paese_content(self, content):
         name = Selector(text=content).xpath('//div[@class="threadlist_text threadlist_title j_th_tit  "]/a/text()').extract()[0]
@@ -30,5 +31,10 @@ class tiebaSpider(scrapy.Spider):
             content_info = self.paese_content(one_content)
             item['post_name'], item['post_author'], item['last_repy_time'] = content_info[0], content_info[1], content_info[2]
             yield item
+        if self.index_url < 100:
+            self.index_url += 50
+            complete_url = self.pre_url + str(self.index_url)
+            yield Request(complete_url)
+            
         
             
